e39a71adbb6cebf0047192dd4f39a30507a38dfd
Javier Perez <surtich@gmail.com>
2015-11-19 21:19:58 +0100

    Fixed a typo

--- src/components/NotificationsDetail.jsx
+++ src/components/NotificationsDetail.jsx
@@ -15,11 +15,11 @@ export default class NotificationsDetail extends Component {
     return (
       <div className="col-md-6">
       	<h3>Pending messages</h3>
-        { messages.map( (messsage, index) => {
-          const className = messsage.isNew ? 'alert alert-info' : 'alert alert-success';
+        { messages.map( (message, index) => {
+          const className = message.isNew ? 'alert alert-info' : 'alert alert-success';
           return (<div key={index} className={className} role="alert">
             <button type="button" className="close" onClick={ () => this.handleRemoveButtonClick(index)}><span>&times;</span></button>
-            <strong>Info:</strong> {messsage.text} Created: {messsage.created.toUTCString()}
+            <strong>Info:</strong> {message.text} Created: {message.created.toUTCString()}
           </div>);
           }
         ) }
